54ec1fdee74de56796892806a8d3f8c9edeeafc5
ikostan <igorkostan@gmail.com>
2019-07-10 14:28:34 -0700

    Edge webdriver
    Edge webdriver is added to the library - tested and works fine

---
+++ basic_driver_test/edge_driver_test.py
@@ -0,0 +1,15 @@
+import unittest
+from selenium import webdriver
+from config import edge_web_driver_path, test_title, test_url
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_edge_driver_works(self):
+
+        driver = webdriver.Edge(edge_web_driver_path)
+        driver.get(test_url)
+        title = driver.title
+        driver.close()
+        self.assertEqual(test_title, title)
+
