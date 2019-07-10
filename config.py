2f05b44f6057f5d09ec0d1502da51f1592cc930b
ikostan <igorkostan@gmail.com>
2019-07-10 15:12:56 -0700

    Mozilla webdriver works fine

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
-chrome_web_driver_path = 'C:/Users/superadmin/Desktop/Python/selenium/' \
-                         'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS/webdriver/' \
-                         'chrome/win32/74.0.3729.6/chromedriver.exe'
+chrome_web_driver_path = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
+                         'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\webdriver\\' \
+                         'chrome\\win32\\74.0.3729.6\\chromedriver.exe'
 
-edge_web_driver_path = 'C:/Users/superadmin/Desktop/Python/selenium/' \
-                       'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS/webdriver/' \
-                       'microsoft_edge/release_17134/MicrosoftWebDriver.exe'
+edge_web_driver_path = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
+                       'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\webdriver\\' \
+                       'microsoft_edge\\release_17134\\MicrosoftWebDriver.exe'
+
+mozilla_web_driver_path = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
+                          'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\' \
+                          'webdriver\\mozilla_geckodriver\\v0.24.0\\win64\\geckodriver.exe'
 
 test_url = 'https://www.ultimateqa.com/automation/'
 
