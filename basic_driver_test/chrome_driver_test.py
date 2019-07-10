d5304c52174e2d5c920a82318426ba1f724f998f
ikostan <igorkostan@gmail.com>
2019-07-10 14:27:55 -0700

    Chrome webdriver
    Chrome webdriver added to the library + tested and works fine

---
+++ basic_driver_test/chrome_driver_test.py
@@ -0,0 +1,15 @@
+import unittest
+from selenium import webdriver
+from config import chrome_web_driver_path, test_title, test_url
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_chrome_driver_works(self):
+
+        driver = webdriver.Chrome(chrome_web_driver_path)
+        driver.get(test_url)
+        title = driver.title
+        driver.close()
+        self.assertEqual(test_title, title)
+
