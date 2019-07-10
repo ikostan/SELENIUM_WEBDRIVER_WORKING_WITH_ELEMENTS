f0416a7bf8b9b03d90e836f88467da94840a52bd
ikostan <igorkostan@gmail.com>
2019-07-10 14:26:47 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+chrome_web_driver_path = 'C:/Users/superadmin/Desktop/Python/selenium/' \
+                         'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS/webdriver/' \
+                         'chrome/win32/74.0.3729.6/chromedriver.exe'
+
+edge_web_driver_path = 'C:/Users/superadmin/Desktop/Python/selenium/' \
+                       'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS/webdriver/' \
+                       'microsoft_edge/release_17134/MicrosoftWebDriver.exe'
+
+test_url = 'https://www.ultimateqa.com/automation/'
+
+test_title = 'Automation Practice - Ultimate QA'
+
