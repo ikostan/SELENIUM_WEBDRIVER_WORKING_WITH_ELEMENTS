e48b59362de25e3bd1d64f4e099660331862c993
ikostan <igorkostan@gmail.com>
2019-07-10 17:57:51 -0700

    Opera driver issue fixed

--- basic_driver_test/opera_driver_test.py
+++ basic_driver_test/opera_driver_test.py
@@ -1,6 +1,6 @@
 import unittest
 from selenium import webdriver
-from config import test_title, test_url
+from config import test_title, test_url, opera_web_driver_path, opera_binary_path
 from selenium.webdriver.opera.options import Options
 
 
@@ -9,11 +9,7 @@ class MyTestCase(unittest.TestCase):
     def test_opera_driver_works(self):
 
         options = Options()
-        options.binary_location = r'C:\\Users\\superadmin\\AppData\\Local\\' \
-                                  r'Programs\\Opera\\60.0.3255.95\\opera.exe'
-        opera_web_driver_path = r'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
-                                r'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\webdriver\\' \
-                                r'opera\\win_64\\2.45\\operadriver.exe'
+        options.binary_location = opera_binary_path
         driver = webdriver.Opera(options=options, executable_path=opera_web_driver_path)
         driver.get(test_url)
         title = driver.title
--- config.py
+++ config.py
@@ -14,9 +14,12 @@ ie_web_driver_path = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
                      'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\webdriver\\' \
                      'IE_Driver_Server\\x64\\3.14.0\\IEDriverServer.exe'
 
-opera_web_driver_path = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\webdriver\\opera\\win_64\\2.45\\operadriver.exe'
+opera_web_driver_path = r'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
+                        'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\webdriver\\' \
+                        'opera\\win_64\\2.45\\operadriver.exe'
 
-#opera_web_driver_path32 = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\webdriver\\opera\\win_32\\2.45\\operadriver.exe'
+opera_binary_path = r'C:\\Users\\superadmin\\AppData\\Local\\' \
+                    r'Programs\\Opera\\60.0.3255.95\\opera.exe'
 
 test_url = 'https://www.ultimateqa.com/automation/'
 
