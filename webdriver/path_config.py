6ab3c0c1076a169067a179a5baa25dbda86d1418
ikostan <igorkostan@gmail.com>
2019-07-12 11:40:12 -0700

    Update path_config.py

--- webdriver/path_config.py
+++ webdriver/path_config.py
@@ -1,26 +1,26 @@
 class DriverPath:
 
-    chrome_web_driver_path = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
+    CHROME_WEB_DRIVER_PATH = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
                              'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\webdriver\\' \
                              'chrome\\win32\\74.0.3729.6\\chromedriver.exe'
 
-    edge_web_driver_path = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
+    EDGE_WEB_DRIVER_PATH = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
                            'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\webdriver\\' \
                            'microsoft_edge\\release_17134\\MicrosoftWebDriver.exe'
 
-    mozilla_web_driver_path = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
+    MOZILLA_WEB_DRIVER_PATH = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
                               'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\' \
                               'webdriver\\mozilla_geckodriver\\v0.24.0\\win64\\geckodriver.exe'
 
-    ie_web_driver_path = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
+    IE_WEB_DRIVER_PATH = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
                          'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\webdriver\\' \
                          'IE_Driver_Server\\x64\\3.14.0\\IEDriverServer.exe'
 
-    opera_web_driver_path = r'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
+    OPERA_WEB_DRIVER_PATH = r'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
                             'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\webdriver\\' \
                             'opera\\win_64\\2.45\\operadriver.exe'
 
-    opera_binary_path = r'C:\\Users\\superadmin\\AppData\\Local\\' \
+    OPERA_BINARY_PATH = r'C:\\Users\\superadmin\\AppData\\Local\\' \
                         r'Programs\\Opera\\60.0.3255.95\\opera.exe'
 
 
