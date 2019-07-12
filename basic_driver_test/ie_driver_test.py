0741246f1bcc88e021d9ed91411830fd68a6a174
ikostan <igorkostan@gmail.com>
2019-07-11 19:31:23 -0700

    IE driver problems solved

--- basic_driver_test/ie_driver_test.py
+++ basic_driver_test/ie_driver_test.py
@@ -8,10 +8,17 @@ class MyTestCase(unittest.TestCase):
     def test_ie_driver_works(self):
 
         driver = webdriver.Ie(executable_path=ie_web_driver_path)
+
+        driver.maximize_window()
+        driver.implicitly_wait(30)
+
         driver.get(test_url)
         url = driver.current_url
         title = driver.title
-        driver.close()
+
         self.assertEqual(test_url, url)
         self.assertEqual(test_title, title)
+
+        driver.close()
+        driver.quit()
 
