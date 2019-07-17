ad3de583833df0aff24430f530a3d4656484187c
ikostan <igorkostan@gmail.com>
2019-07-17 01:01:57 -0700

    Update test_template.py

--- testing_selenium_capabilities/tests/test_template.py
+++ testing_selenium_capabilities/tests/test_template.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support import expected_conditions
 
 
 class MyTestCase(unittest.TestCase):
