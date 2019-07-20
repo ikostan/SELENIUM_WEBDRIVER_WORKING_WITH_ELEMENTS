cbf73fd37e52f2c29fbf3f079011b320458139ca
ikostan <igorkostan@gmail.com>
2019-07-20 15:58:08 -0700

    Update path_config.py

--- drivers/path_config.py
+++ drivers/path_config.py
@@ -4,21 +4,17 @@ class DriverPath:
                              'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\drivers\\' \
                              'chrome\\win32\\74.0.3729.6\\chromedriver.exe'
 
-    EDGE_WEB_DRIVER_PATH = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
-                           'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\drivers\\' \
-                           'microsoft_edge\\release_17134\\MicrosoftWebDriver.exe'
+    EDGE_WEB_DRIVER_PATH = '\\drivers\\microsoft_edge\\release_17134\\MicrosoftWebDriver.exe'
 
-    MOZILLA_WEB_DRIVER_PATH = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
-                              'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\' \
-                              'drivers\\mozilla_geckodriver\\v0.24.0\\win64\\geckodriver.exe'
+    MOZILLA_WEB_DRIVER_PATH = '\\drivers\\mozilla_geckodriver\\v0.24.0\\win64\\geckodriver.exe'
 
     IE_WEB_DRIVER_PATH = 'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
                          'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\drivers\\' \
                          'IE_Driver_Server\\x86\\3.14.0\\IEDriverServer.exe'
 
     OPERA_WEB_DRIVER_PATH = r'C:\\Users\\superadmin\\Desktop\\Python\\selenium\\' \
-                            'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\drivers\\' \
-                            'opera\\win_64\\2.45\\operadriver.exe'
+                            r'SELENIUM_WEBDRIVER_WORKING_WITH_ELEMENTS\\drivers\\' \
+                            r'opera\\win_64\\2.45\\operadriver.exe'
 
     OPERA_BINARY_PATH = r'C:\\Users\\superadmin\\AppData\\Local\\' \
                         r'Programs\\Opera\\60.0.3255.95\\opera.exe'
