b8473f00240d3c1d04f073f731c0a9ed3466131b
ikostan <igorkostan@gmail.com>
2019-07-23 19:05:04 -0700

    Create 3_3_locating_by_xpath_test.py

---
+++ testing_selenium_capabilities/tests/locating_elements/3_3_locating_by_xpath_test.py
@@ -0,0 +1,77 @@
+import unittest
+from drivers.driver import Driver
+import time
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+
+class MyTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = None
+        cls.test_title = 'PHPTRAVELS | Travel Technology Partner'
+        cls.test_url = 'https://www.phptravels.net/'
+
+    def setUp(self):
+        self.driver = None
+
+    def test_chrome(self):
+        browser = 'chrome'
+        self.generic_method(browser)
+
+    def test_ie(self):
+        browser = 'ie'
+        self.generic_method(browser)
+
+    def test_opera(self):
+        browser = 'opera'
+        self.generic_method(browser)
+
+    def test_mozilla(self):
+        browser = 'mozilla'
+        self.generic_method(browser)
+
+    def test_edge(self):
+        browser = 'edge'
+        self.generic_method(browser)
+
+    def generic_method(self, browser):
+
+        # open test webpage
+        self.open_test_web_page(browser)
+
+        # detect 'My Account' menu item and click on it:
+        drop_down_menu = self.driver.find_element(By.XPATH,
+                                                  '/html/body/nav/div/div[2]/ul[2]/ul/li[1]/a')
+        drop_down_menu.click()
+
+        # verify that submenu items are visible
+        self.assertEqual('true', drop_down_menu.get_attribute('aria-expanded'))
+        time.sleep(1)
+
+        # Hit 'My Account' again and verify that submenu items are invisible
+        drop_down_menu.click()
+        self.assertEqual('false', drop_down_menu.get_attribute('aria-expanded'))
+        time.sleep(1)
+
+    def open_test_web_page(self, browser):
+        # Open test web page and verify URL + Title
+        self.driver = Driver(browser).get_driver()
+        self.driver.get(self.test_url)
+        self.driver.maximize_window()
+        time.sleep(1)
+        self.assertEqual(self.test_url, self.driver.current_url)
+        self.assertEqual(self.test_title, self.driver.title)
+
+    def tearDown(self):
+        self.driver.close()
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.driver is not None:
+            cls.driver.quit()
