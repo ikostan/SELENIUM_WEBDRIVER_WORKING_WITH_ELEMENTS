9201706138282ccc3c8bfdc86c45377696b2b138
Egor Kostan <igorkostan@gmail.com>
2019-07-25 16:03:45 -0700

    Update 2_3_drag_and_drop_test.py

--- testing_selenium_capabilities/tests/navigating/2_3_drag_and_drop_test.py
+++ testing_selenium_capabilities/tests/navigating/2_3_drag_and_drop_test.py
@@ -44,6 +44,28 @@ class MyTestCase(unittest.TestCase):
 
     def generic_method(self, browser):
 
+        self.open_web_page(browser)
+
+        iframe = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/iframe')
+        self.driver.switch_to.frame(iframe)
+
+        source = self.driver.find_element_by_xpath('//*[@id="draggable"]')
+        target = self.driver.find_element_by_id('droppable')
+        self.assertEqual("Drop here", target.text)
+        time.sleep(2)
+
+        actions = ActionChains(self.driver)
+        actions.drag_and_drop(source, target).perform()
+        time.sleep(2)
+        try:
+            self.assertEqual("Dropped!", target.text)
+        except Exception as e:
+            print('\nERROR: ', e.args)
+            self.screen_shot()
+            raise
+
+    def open_web_page(self, browser):
+
         try:
             # Open web browser and verify page title:
             self.driver = Driver(browser).get_driver()
@@ -70,24 +92,6 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.test_url, self.driver.current_url)
             self.assertEqual(self.test_title, self.driver.title)
 
-        iframe = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/iframe')
-        self.driver.switch_to.frame(iframe)
-
-        source = self.driver.find_element_by_xpath('//*[@id="draggable"]')
-        target = self.driver.find_element_by_id('droppable')
-        self.assertEqual("Drop here", target.text)
-        time.sleep(2)
-
-        actions = ActionChains(self.driver)
-        actions.drag_and_drop(source, target).perform()
-        time.sleep(2)
-        try:
-            self.assertEqual("Dropped!", target.text)
-        except Exception as e:
-            print('\nERROR: ', e.args)
-            self.screen_shot()
-            raise
-
     def screen_shot(self):
         """Take a Screen-shot of the webpage when test Failed."""
         now = datetime.datetime.now()
@@ -110,17 +114,17 @@ class MyTestCase(unittest.TestCase):
 
         now = datetime.datetime.now()
         folder_name = '{}\\screenshots_{}'.format(screenshots_folder, datetime.datetime.strftime(now, '%Y-%m-%d_%H-%M-%S'))
-        os.mkdir(folder_name)
 
         files = os.listdir(os.curdir)
         for file in files:
             if '.png' in str(file):
+                if not os.path.exists(os.curdir + '\\' + folder_name):
+                    os.mkdir(folder_name)
                 shutil.move(file.split('\\')[-1], os.curdir + '\\' + folder_name)
 
     def tearDown(self):
         self.driver.stop_client()
         self.driver.close()
-        time.sleep(1)
 
     @classmethod
     def tearDownClass(cls):
