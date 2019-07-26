b6b43b40277de49dc2c6d761a16fc30c4b564aec
Egor Kostan <igorkostan@gmail.com>
2019-07-25 17:18:30 -0700

    Update 2_5_moving_between_frames_test.py

--- testing_selenium_capabilities/tests/navigating/2_5_moving_between_frames_test.py
+++ testing_selenium_capabilities/tests/navigating/2_5_moving_between_frames_test.py
@@ -51,6 +51,76 @@ class MyTestCase(unittest.TestCase):
 
     def generic_method(self, browser):
 
+        self.open_web_browser(browser)
+
+        # detect button
+        single_iframe_btn = self.driver.find_element(By.XPATH, self.single_iframe_btn_xpath)
+
+        # Click on 'Single iframe' button >>>
+        # enter single iframe >>>
+        single_iframe_btn.click()
+        single_iframe = self.driver.find_element(By.ID, self.single_iframe_id)
+        self.driver.switch_to.frame(single_iframe)
+
+        # write text (single iframe) inside text field >>>
+        # exit iframe
+        txt_field = self.driver.find_element(By.XPATH, self.text_field_xpath)
+        txt_field.send_keys('single iframe')
+        time.sleep(1)
+        self.assertEqual('single iframe', txt_field.get_attribute('value'))
+        self.driver.switch_to.default_content()
+
+        # detect button + click on it
+        multiple_iframes_btn = self.driver.find_element(By.XPATH, self.multiple_iframes_btn_xpath)
+        multiple_iframes_btn.click()
+
+        # detect + switch to multiple iframe
+        # implemented with wait due to slow performance of IE and FireFox
+        wait = WebDriverWait(self.driver, 5)
+
+        try:
+            wait.until(expected_conditions.frame_to_be_available_and_switch_to_it(self.driver.find_element(By.XPATH,
+                                                                                  self.multiple_frames_xpath)))
+        except TimeoutException as ec:
+            print('\n', ec)
+            print('\nTrying to refresh...')
+            self.driver.refresh()
+            wait.until(expected_conditions.frame_to_be_available_and_switch_to_it(self.driver.find_element(By.XPATH,
+                                                                                  self.multiple_frames_xpath)))
+
+        # detect inner frame + switch to it
+        inner_frame = self.driver.find_element(By.XPATH, self.single_inside_multiple_iframe_xpath)
+        self.driver.switch_to.frame(inner_frame)
+
+        # write text (single iframe) inside text field >>>
+        # exit iframe
+        try:
+            WebDriverWait(self.driver, 5).until(expected_conditions.element_to_be_clickable((By.XPATH,
+                                                                                             self.text_field_xpath)))
+        except TimeoutException as ec:
+            print('\n', ec)
+            self.driver.switch_to.default_content()
+
+            # detect inner frame + switch to it
+            self.driver.switch_to.frame(inner_frame)
+            WebDriverWait(self.driver, 5).until(expected_conditions.element_to_be_clickable((By.XPATH,
+                                                                                             self.text_field_xpath)))
+
+        txt_field = self.driver.find_element(By.XPATH, self.text_field_xpath)
+
+        txt_field.send_keys('inner iframe')
+        time.sleep(1)
+        self.assertEqual('inner iframe', txt_field.get_attribute('value'))
+        self.driver.switch_to.default_content()
+
+        # click on iframe buttons back and forward:
+        single_iframe_btn.click()
+        time.sleep(1)
+        multiple_iframes_btn.click()
+        time.sleep(1)
+
+    def open_web_browser(self, browser):
+
         try:
             # Launch webdriver on test web page + maximize window:
             self.driver = Driver(browser).get_driver()
@@ -76,73 +146,6 @@ class MyTestCase(unittest.TestCase):
             # Verify page title + url
             self.assertEqual(self.url, self.driver.current_url)
             self.assertEqual(self.window_name, self.driver.title)
-
-        # detect button
-        single_iframe_btn = self.driver.find_element(By.XPATH, self.single_iframe_btn_xpath)
-
-        # Click on 'Single iframe' button >>>
-        # enter single iframe >>>
-        single_iframe_btn.click()
-        single_iframe = self.driver.find_element(By.ID, self.single_iframe_id)
-        self.driver.switch_to.frame(single_iframe)
-
-        # write text (single iframe) inside text field >>>
-        # exit iframe
-        txt_field = self.driver.find_element(By.XPATH, self.text_field_xpath)
-        txt_field.send_keys('single iframe')
-        time.sleep(1)
-        self.assertEqual('single iframe', txt_field.get_attribute('value'))
-        self.driver.switch_to.default_content()
-
-        # detect button + click on it
-        multiple_iframes_btn = self.driver.find_element(By.XPATH, self.multiple_iframes_btn_xpath)
-        multiple_iframes_btn.click()
-
-        # detect + switch to multiple iframe
-        # implemented with wait due to slow performance of IE and FireFox
-        wait = WebDriverWait(self.driver, 20)
-
-        try:
-            wait.until(expected_conditions.frame_to_be_available_and_switch_to_it(self.driver.find_element(By.XPATH,
-                                                                                  self.multiple_frames_xpath)))
-        except TimeoutException as ec:
-            print('\n', ec)
-            print('\nTrying to refresh...')
-            self.driver.refresh()
-            wait.until(expected_conditions.frame_to_be_available_and_switch_to_it(self.driver.find_element(By.XPATH,
-                                                                                  self.multiple_frames_xpath)))
-
-        # detect inner frame + switch to it
-        inner_frame = self.driver.find_element(By.XPATH, self.single_inside_multiple_iframe_xpath)
-        self.driver.switch_to.frame(inner_frame)
-
-        # write text (single iframe) inside text field >>>
-        # exit iframe
-        try:
-            WebDriverWait(self.driver, 20).until(expected_conditions.element_to_be_clickable((By.XPATH,
-                                                                                              self.text_field_xpath)))
-        except TimeoutException as ec:
-            print('\n', ec)
-            self.driver.switch_to.default_content()
-
-            # detect inner frame + switch to it
-            #inner_frame = self.driver.find_element(By.XPATH, self.single_inside_multiple_iframe_xpath)
-            self.driver.switch_to.frame(inner_frame)
-            WebDriverWait(self.driver, 20).until(expected_conditions.element_to_be_clickable((By.XPATH,
-                                                                                              self.text_field_xpath)))
-
-        txt_field = self.driver.find_element(By.XPATH, self.text_field_xpath)
-
-        txt_field.send_keys('inner iframe')
-        time.sleep(1)
-        self.assertEqual('inner iframe', txt_field.get_attribute('value'))
-        self.driver.switch_to.default_content()
-
-        # click on iframe buttons back and forward:
-        single_iframe_btn.click()
-        time.sleep(1)
-        multiple_iframes_btn.click()
-        time.sleep(1)
 
     def tearDown(self):
         for handle in self.driver.window_handles:
